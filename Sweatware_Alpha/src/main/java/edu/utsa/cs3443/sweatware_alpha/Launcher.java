b4173af22b430e051de41184abf948ebddced882
Shiro-Garza <kade.garza@my.utsa.edu>
2025-11-05 17:10:08 -0600

    Add files via upload

---
+++ Sweatware_Alpha/src/main/java/edu/utsa/cs3443/sweatware_alpha/Launcher.java
@@ -0,0 +1,9 @@
+package edu.utsa.cs3443.sweatware_alpha;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(SweatwareApplication.class, args);
+    }
+}
