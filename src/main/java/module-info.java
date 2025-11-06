ae2639a91144b6b679e44f14f0c497048e9967b3
Shiro-Garza <kade.garza@my.utsa.edu>
2025-11-05 20:47:08 -0600

    :0

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module edu.utsa.cs3443.sweatware_alpha {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires jdk.httpserver;
+
+
+    opens edu.utsa.cs3443.sweatware_alpha to javafx.fxml;
+    exports edu.utsa.cs3443.sweatware_alpha;
+}
